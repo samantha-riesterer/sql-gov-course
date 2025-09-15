f6223bc975494a5685f942dca5453dc14565fbe3
Samantha Riesterer <riesterersamantha@gmail.com>
2025-09-15 13:55:34 -0700

    Completed Part 2 Q1.1, including data validation & analysis notes

--- budget-analysis/sql-scripts/budget_performance_analysis.sql
+++ budget-analysis/sql-scripts/budget_performance_analysis.sql
@@ -33,21 +33,22 @@ annual_budgets AS (
         a.agency_id,
         a.agency_name, 
         SUM(ba.budgeted_amount) AS total_budget,
-        SUM(ba.quarter_1_target) AS total_target
+        SUM(ba.quarter_1_target) AS q1_target
     FROM agencies a 
     JOIN budget_allocations ba ON a.agency_id = ba.agency_id
     WHERE ba.fy_id = 2
     GROUP BY a.agency_name,a.agency_id
 ),
 
--- calculates variance data using Q1 expenditure data and Q1 target budget, then project for annual expenditure
+-- calculates variance data using Q1 expenditure data and Q1 target budget 
+-- then project for annual expenditure
 variance_data AS (
     SELECT
         ab.agency_name,
         ab.total_budget,
-        qd.q1_actual - ab.total_target AS variance,
-        (qd.q1_actual - ab.total_target) / ab.total_target AS variance_rate,
-        ab.total_budget * (1 + ((qd.q1_actual - ab.total_target) / ab.total_target)) AS annual_projection
+        qd.q1_actual - ab.q1_target AS variance,
+        (qd.q1_actual - ab.q1_target) / ab.q1_target AS variance_rate,
+        ab.total_budget * (1 + ((qd.q1_actual - ab.q1_target) / ab.q1_target)) AS annual_projection
     FROM annual_budgets ab
     JOIN q1_data qd ON ab.agency_id = qd.agency_id
 )
@@ -64,10 +65,24 @@ SELECT
 FROM variance_data vd
 ORDER BY variance_rate DESC;
 
---Data Validation Check 
---Are your total_target values reasonable? (Should be roughly 25% of annual budget if spending is evenly distributed)
---Do your variance rates make sense? (Should be decimals like 0.15 or -0.20, not huge numbers)
---Are agencies with positive variance rates showing projected expenditures above their annual budget?
 
 --ANALYSIS RESULTS
---
+-- Dept. of Commerce approx. 35% over budget as of Q1, projected to be approx.$56.5 million over annual budget 
+-- Every other department under budget as of Q1, with Dept. of L&I approx. 71% under budget
+
+--DATA VALIDATION
+--total_target values are ~20-25% of total budget 
+--variance rates are decimal values 
+--positive/negative rates corresponding to over or under budget projections
+/*
+--Data Validation Test
+SELECT 
+    ab.agency_name,
+    qd.q1_actual, 
+    ab.q1_target,
+    ab.total_budget,
+   (ab.q1_target / ab.total_budget) * 100  AS percent_of_budget
+   FROM annual_budgets ab
+   JOIN q1_data qd ON ab.agency_id = qd.agency_id;
+*/
+
